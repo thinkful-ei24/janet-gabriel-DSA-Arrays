ffdeed9e4319951f94005785ecdfefd8737f8cd9
Janet Leon <leon.irenejanet@gmail.com>
2018-11-07 11:34:30 -0800

    completed firs two array drills

--- array-drills.js
+++ array-drills.js
@@ -2,17 +2,46 @@
 /* eslint-disable indent */
 //URLify a string
 
-const URLify = str=>{
-    let newUrl= '';
+//Big O is o(n) Linear
+// the run time complexity scales with the iput size
+// const URLify = str=>{
+//     let newUrl= '';
 
-    for (let i=0; i<str.length; i++){
-        if(str[i] !== ' '){
-            newUrl += str[i];
-        } else {
-            newUrl += '%20';
-        }
-    }
-    return newUrl;
+//     for (let i=0; i<str.length; i++){
+//         if(str[i] !== ' '){
+//             newUrl += str[i];
+//         } else {
+//             newUrl += '%20';
+//         }
+//     }
+//     return newUrl;
+// };
+
+// console.log(URLify('www.thinkful.com /tauh ida parv een'));
+
+//Filtering an array
+
+//option1
+// const filterArr = (arr, minValue)=>{
+//     const filteredArr= [];
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > minValue){
+//             filteredArr.push(arr[i])
+//         }
+//     }
+
+//     return filteredArr;
+// };
+
+//option2
+const filterArr = (arr, minValue)=>{
+     const filteredArray= [];
+
+     arr.forEach(value=> value > minValue ? filteredArray.push(value) : null );
+
+     return filteredArray;
 };
+  
 
-console.log(URLify('www.thinkful.com /tauh ida parv een'));
+console.log(filterArr([1,2,3,4,5,6,7,8], 5));
